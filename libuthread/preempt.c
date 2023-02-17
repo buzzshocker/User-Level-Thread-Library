b196a5d8621be6226337465f9809fbed7ff732c8
Japman Singh Kohli <91647755+buzzshocker@users.noreply.github.com>
2023-02-17 15:11:24 -0800

    Updated preempt.c for phase 4

--- libuthread/preempt.c
+++ libuthread/preempt.c
@@ -15,6 +15,16 @@
  */
 #define HZ 100
 
+struct sigaction new_action, old_action;
+struct itimerval new_timer, old_timer;
+int signum;
+
+void signal_handler(int signum) {
+    (void)signum;
+    signum = SIGVTALRM;
+    uthread_yield();
+}
+
 void preempt_disable(void)
 {
 	/* TODO Phase 4 */
@@ -25,13 +35,27 @@ void preempt_enable(void)
 	/* TODO Phase 4 */
 }
 
-// void preempt_start(bool preempt)
-// {
-// 	/* TODO Phase 4 */
-// }
+void preempt_start(bool preempt)
+{
+	/* TODO Phase 4 */
+    new_action.sa_handler = signal_handler;
+    sigemptyset(&new_action.sa_mask);
+    new_action.sa_flags = 0;
+
+    new_timer.it_value.tv_sec = HZ/1000;  // Converts HZ to seconds
+    new_timer.it_value.tv_usec = (1000*HZ)%1000000;  // Converts HZ to milliseconds
+    new_timer.it_interval = new_timer.it_value;
+
+    if (preempt == true) {
+        sigaction(signum, &new_action, &old_action);
+        setitimer(ITIMER_VIRTUAL, &new_timer, &old_timer);
+    }
+}
 
 void preempt_stop(void)
 {
 	/* TODO Phase 4 */
+    sigaction(signum, &old_action, &new_action);
+    setitimer(ITIMER_VIRTUAL, &old_timer, &new_timer);
 }
 
