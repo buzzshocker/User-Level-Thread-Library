11a4205e7086694525b2bb98cd9743ec8ce7001c
Japman Singh Kohli <91647755+buzzshocker@users.noreply.github.com>
2023-02-20 18:59:47 -0800

    Update preempt tester

--- apps/test_preempt.c
+++ apps/test_preempt.c
@@ -1,27 +1,28 @@
-//
-// Created by japma on 2/17/2023.
-//
-
 #include <private.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <uthread.h>
+#include <signal.h>
+
+int inf_val = 0;
 
 void exit_thread(void* arg) {
     (void)arg;
-    printf("Now we have entered the thread that will exit and "
+    printf("inf_val = %d\n", inf_val);
+    printf("Now we have entered the thread that will raise SIGINT and "
            "end this execution. Bye!\n");
-    exit(0);
+    raise(SIGINT);
 }
 
 void preempt_thread(void* arg) {
     (void)arg;
     printf("The thread that will run infinitely starts.\n");
     uthread_create(exit_thread, arg);
-    printf("Now we start the infinite loop.\n");
-    int inf_val;
-    while(1) {
+    printf("Now we start the infinite loop that increments and decrements "
+           "a global value - inf_value.\n");
+    while(inf_val >= 0) {
         inf_val++;
+        inf_val--;
     }
 }
 
