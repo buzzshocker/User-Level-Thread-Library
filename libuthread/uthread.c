19199da65ffb01f55ba8c727d650e5758cad93a7
Japman Singh Kohli <91647755+buzzshocker@users.noreply.github.com>
2023-02-21 18:06:11 -0800

    Updated uthread.c

--- libuthread/uthread.c
+++ libuthread/uthread.c
@@ -30,6 +30,8 @@ struct uthread_tcb *uthread_current(void) {
 }
 
 void uthread_yield(void) {
+    // Disable preemption while entering critical section
+    preempt_disable();
     // Push the currently running thread into the queue
     queue_enqueue(thread_queue, current_thread);
     // Then pop the next thread from the queue for context switching
@@ -41,9 +43,13 @@ void uthread_yield(void) {
     current_thread = next_thread;
     // Context switch between currently running thread and the next thread
     uthread_ctx_switch(curr_node->uctx, next_thread->uctx);
+    // Enable preemption while exiting the critical section
+    preempt_enable();
 }
 
 void uthread_exit(void) {
+    // Disable preemption while entering critical section
+    preempt_disable();
     // Pop the next thread from the thread_queue
     queue_dequeue(thread_queue, (void **)&next_thread);
     // Creating a temporary TCB that has a copy of the current thread
@@ -55,6 +61,8 @@ void uthread_exit(void) {
     queue_enqueue(exited_thread_queue, dq_node);
     // Context switch between currently running thread and the next thread
     uthread_ctx_switch(dq_node->uctx, current_thread->uctx);
+    // Enable preemption while exiting the critical section
+    preempt_enable();
 }
 
 int uthread_create(uthread_func_t func, void *arg) {
@@ -69,11 +77,11 @@ int uthread_create(uthread_func_t func, void *arg) {
     t1->top_of_stack = uthread_ctx_alloc_stack();
     // Initialize t1's execution context
     uthread_ctx_init(t1->uctx, t1->top_of_stack, func, arg);
-    // Enable preemption as we are done with critical section
-    preempt_enable();
     // Make t1 the next_thread and enqueue it into the thread queue
     next_thread = t1;
     queue_enqueue(thread_queue, t1);
+    // Enable preemption as we are done with critical section
+    preempt_enable();
     return 0;
 }
 
@@ -113,6 +121,8 @@ int uthread_run(bool preempt, uthread_func_t func, void *arg) {
 }
 
 void uthread_block(void) {
+    // Disable preemption as we enter critical section
+    preempt_disable();
     struct uthread_tcb *dq_thread;
     // Dequeue the first ready thread from thread_queue
     queue_dequeue(thread_queue, (void **)&dq_thread);
@@ -123,10 +133,16 @@ void uthread_block(void) {
     current_thread = dq_thread;
     // Context switch from that current_thread to the dequeued thread
     uthread_ctx_switch(curr_thread->uctx, dq_thread->uctx);
+    // Enable preemption as we are done with critical section
+    preempt_enable();
 }
 
 void uthread_unblock(struct uthread_tcb *uthread) {
+    // Disable preemption as we enter critical section
+    preempt_disable();
     // Unblocking the thread that was previously blocked by enqueuing into the
     // thread_queue
     queue_enqueue(thread_queue, uthread);
+    // Enable preemption as we are done with critical section
+    preempt_enable();
 }
